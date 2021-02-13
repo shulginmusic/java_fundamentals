209e202605c5978239c042131ea88b172c007246
Efim Shulgin <efim@shulginmusic.com>
2021-02-13 22:20:00 +0400

    Shortened the WordCounter using Files utility class

--- src/labs_examples/input_output/labs/WordCounter.java
+++ src/labs_examples/input_output/labs/WordCounter.java
@@ -1,6 +1,10 @@
 package labs_examples.input_output.labs;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 //A Simple Java program to find out how many times a word appears in a certain text file
 public class WordCounter {
@@ -14,24 +18,13 @@ public class WordCounter {
         //Get input from user
         Scanner scanner = new Scanner(System.in);
         String userWord = scanner.next();
-        //Instantiate the File and FileReader objects
-        File textFile = null;
-        FileReader fileIn = null;
         //Declare the wordCount variable
         int wordCount = 0;
         try {
-            //Declare the File and FileReader objects
-            textFile = new File(path);
-            fileIn = new FileReader(textFile);
-            //Declare the char array to hold the data from the file
-            char [] fileChars = new char[(int) textFile.length()];
-            //Read the chars into the char array
-            fileIn.read(fileChars);
-            //Make a string to hold the text not
-            String text = new String(fileChars);
-            //Make a words array
-            //Use a regex to only put letters/nums into words arr
-            String [] words = text.split("\\W+");
+            //Declare the string array to hold the words
+            //Read the text in using the Files utility class, then split the string into the array utilizing a regex
+            //So it will be words only
+            String [] words = new String(Files.readAllBytes(Paths.get(path))).split("\\W+");
             //Check if a word appears and if so, how many times
             for (String word : words) {
                 //Ignore the case
@@ -41,13 +34,6 @@ public class WordCounter {
             }
         } catch (IOException exc) {
             System.out.println(exc.getLocalizedMessage());
-        } finally {
-            try {
-                assert fileIn != null;
-                fileIn.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
         //Let the user know the result
         System.out.println("The word " + userWord + " appears in the file " + wordCount + " times");
