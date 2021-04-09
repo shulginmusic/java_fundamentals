302aab4c08f282b7af0942683935d72e2b7f0633
shulginmusic <efim@shulginmusic.com>
2021-04-08 21:27:39 -0700

    Hashmap labs part 1

--- src/labs_examples/datastructures/hashmap/labs/Exercise_01.java
+++ src/labs_examples/datastructures/hashmap/labs/Exercise_01.java
@@ -27,14 +27,58 @@ public class Exercise_01 {
     public static void main(String[] args) {
         HashMap<Integer, String> users = new HashMap<>();
 
-        //put
+        //put()
         users.put(1, "efim1994");
-        users.put(2, "agniia_z");
+        users.put(2, "henrymaas342");
 
-        //get
+        //get()
         System.out.println(users.get(2));
 
-        //putAll
+        //putAll()
+        HashMap<Integer, String> usersTwo = new HashMap<>();
+        usersTwo.putAll(users);
+        System.out.println(users.get(2));
+
+        //size()
+        System.out.println(users.size());
+
+        //contains()
+        System.out.println(users.containsKey(3));
+        System.out.println(users.containsValue("efim1994"));
+
+        //keySet()
+        System.out.println(users.keySet()); //shows the keys in this map
+
+        //entrySet()
+        System.out.println(users.entrySet()); //shows the entries (key-value pairs) in this map
+
+        //putIfAbsent()
+        users.putIfAbsent(3, "devin_t32");
+        users.putIfAbsent(3, "devin_t32");
+        System.out.println(users.entrySet());
+
+        //remove()
+        users.remove(3);
+
+        //replace()
+        users.replace(2, "james_h12");
+        System.out.println(users.get(2));
+
+        //forEach() Thanks to: https://www.programiz.com/java-programming/library/hashmap/foreach
+        users.forEach((key, value) -> {
+            System.out.println("Key: " + key);
+            System.out.println("Value: " + value);
+        });
+
+        //clear()
+        users.clear();
+        System.out.println(users.entrySet()); //entry set is now empty because the map has been cleared
+
+
+
+
+
+
 
 
     }
